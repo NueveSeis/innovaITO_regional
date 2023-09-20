e388b80b4e7536f757d6a4114fd7f547f5b9ab06
NueveSeis <nueveseisss@gmail.com>
2023-09-20 15:01:26 -0600

    capturas de pantalla no permitidas.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:innova_ito/router/app_router.dart';
 import 'package:innova_ito/theme/app_tema.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 //void main() => runApp(const ProviderScope(child: MyApp()));
 void main() async {
   await dotenv.load(fileName: "lib/.env");
+  await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
   //const ProviderScope(child: MyApp());
   runApp(const ProviderScope(child: MyApp()));
 }
